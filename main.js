e2967e9056727c144b5588ba4306c1e4ab3311be
ammarshaikh123 <144041358+ammarshaikh22@users.noreply.github.com>
2026-04-23 01:29:30 +0500

    ads

--- main.js
+++ main.js
@@ -17,13 +17,7 @@ dotenv.config();
 
 const app = express();
 app.use(express.json());
-app.use(
-  cors({
-    origin: ["https://fieldfrontend.vercel.app", "http://localhost:3000"],
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    credentials: true,
-  })
-);
+app.use(cors());
 app.use("/api/v1", Authrouter);
 app.use("/api/v2", adminRouter);
 app.use("/api/v2", clientRouter);
