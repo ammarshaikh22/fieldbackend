499fdd2313d93f70cb3d66f2ded7f31e28037308
ammarshaikh123 <144041358+ammarshaikh22@users.noreply.github.com>
2026-04-23 15:15:01 +0500

    sdas

--- src/utils/mail.js
+++ src/utils/mail.js
@@ -1,26 +1,41 @@
-import { Resend } from "resend";
+import nodemailer from "nodemailer";
 import User from "../models/user.model.js";
 import dotenv from "dotenv";
-dotenv.config();
-const resend = new Resend(process.env.RESEND_API_KEY);
 
-const sendMail = async ({ email, userId }) => {
+dotenv.config();
+
+const sendMail = async (email, userId) => {
   try {
+    // OTP generate
     const otp = Math.floor(100000 + Math.random() * 900000);
 
+    // DB update
     await User.findByIdAndUpdate(userId, {
       otp_token: otp,
-      otp_expiry: Date.now() + 3600000,
+      otp_expiry: Date.now() + 3600000, // 1 hour
     });
 
-    const response = await resend.emails.send({
-     from: "noreply@ammar.fieldops.com",
+    // transporter (Gmail SMTP)
+    const transporter = nodemailer.createTransport({
+      host: "smtp.gmail.com",
+      port: 587,
+      secure: false,
+      auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
+      },
+    });
+
+    const mailOptions = {
+      from: process.env.EMAIL_USER,
       to: email,
-      subject: "Verify Email",
+      subject: "Verify Email OTP",
       html: `<p>Your OTP is: <b>${otp}</b></p>`,
-    });
+    };
 
-    return response;
+    const mail = await transporter.sendMail(mailOptions);
+
+    return mail;
   } catch (error) {
     console.log("MAIL ERROR:", error.message);
     return null;
